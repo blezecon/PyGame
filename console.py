db473ea0fad876961388befbf9f7780dad680a65
xor-xeno <dfyyasdd@gmail.com>
2025-05-28 14:56:11 +0530

    2nd phase

--- console.py
+++ console.py
@@ -1,4 +1,5 @@
 import pygame
+from sys import exit
 
 pygame.init()
 
@@ -6,3 +7,12 @@ SCREEN_WIDTH = 1366
 SCREEN_HEIGHT = 768
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("GAME")
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+
+    pygame.display.flip()  # Update the display
